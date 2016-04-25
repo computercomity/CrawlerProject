595a0a221eb980ec35ce1c4422164141cabfaaba
alesiong <ypf7.10@hotmail.com>
2016-04-25 08:21:52 +0800

    format student.py

--- MoodleTools/student.py
+++ MoodleTools/student.py
@@ -9,13 +9,16 @@ from MoodleTools.moodleParser import MoodleParser
 from MoodleTools.config import StudentConfig
 from MoodleTools.error import *
 
+
 class Student:
     """ The basic class for student
 
     Usage: Init this class with the student id and password which you get from web or input.
     And then use method to do something. You can def your method by your own.
      If you do so, write down your name in the commend of your method """
-    def __init__(self, student_id, password):  # The password may need encryption
+
+    def __init__(self, student_id,
+                 password):  # The password may need encryption
         self.__student_id = student_id
         self.__password = password
         self.config = StudentConfig()
@@ -26,15 +29,17 @@ class Student:
         """ login and get index response """
         username = self.__student_id
         data = {
-            'username':username,
-            'password':self.__password,
+            'username': username,
+            'password': self.__password,
         }
         login_session = requests.Session()
-        login_request = login_session.post(self.config.MOODLE_LOGIN_URL, data=data)
+        login_request = login_session.post(self.config.MOODLE_LOGIN_URL,
+                                           data=data)
         self.cookies = login_request.cookies
-        index_request = login_session.post(self.config.MOODLE_URL, cookies=self.cookies)
+        index_request = login_session.post(self.config.MOODLE_URL,
+                                           cookies=self.cookies)
         return index_request.text
 
     def get_ddl(self):
         pass
-    # write your function here, be care of your code style.
+        # write your function here, be care of your code style.
